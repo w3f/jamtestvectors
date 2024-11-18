078db06d6601c7cf985a1a006e0d75cd15a1546f
Davide Galassi <davxy@datawok.net>
2024-11-18 16:49:03 +0100

    safrole is using shared asn.1 code

--- codec/validate.py
+++ codec/validate.py
@@ -7,10 +7,10 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../asn1-schema')))
 
-from utils import schema_files, validate
+from utils import get_schema_files, validate
 
 
-schema = asn1tools.compile_files(schema_files, codec="jer")
+schema = asn1tools.compile_files(get_schema_files(), codec="jer")
 for path in Path("data").iterdir():
     if path.is_file() and path.suffix == ".json":
         validate(schema, path)
