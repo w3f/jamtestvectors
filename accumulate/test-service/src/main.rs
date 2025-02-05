562f11d129d3a6ab35bfb91ad5973358ae0654fa
Davide Galassi <davxy@datawok.net>
2025-02-05 11:02:45 +0100

    Align to jam-pvm-common 0.1.12

--- accumulate/test-service/src/main.rs
+++ accumulate/test-service/src/main.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use jam_types::{
-    AccumulateItem, PackageInfo, ServiceId, Slot, TransferRecord, WorkOutput, WorkPayload,
+    AccumulateItem, Hash, PackageInfo, ServiceId, Slot, TransferRecord, WorkOutput, WorkPayload,
 };
 
 #[cfg(not(any(target_arch = "riscv32", target_arch = "riscv64")))]
@@ -24,13 +24,14 @@ impl jam_pvm_common::Service for Service {
     ) -> WorkOutput {
         [&b"Hello "[..], payload.take().as_slice()].concat().into()
     }
-    fn accumulate(_slot: Slot, _id: ServiceId, items: Vec<AccumulateItem>) {
+    fn accumulate(_slot: Slot, _id: ServiceId, items: Vec<AccumulateItem>) -> Option<Hash> {
         for item in items.into_iter() {
             if let Ok(data) = item.result {
                 jam_pvm_common::accumulate::set_storage(item.package.as_slice(), &data)
                     .expect("not enough balance?!");
             }
         }
+        None
     }
     fn on_transfer(_slot: Slot, _id: ServiceId, _items: Vec<TransferRecord>) {}
 }
