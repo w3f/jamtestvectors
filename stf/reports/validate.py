1585b1485c2c820d1aa39221d06811a89cf53031
Davide Galassi <davxy@datawok.net>
2025-06-09 18:42:53 +0200

    Fix last one

--- stf/reports/validate.py
+++ stf/reports/validate.py
@@ -6,7 +6,7 @@ import sys
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(script_dir, '../../lib')))
 
-from utils import validate_group # noqa: E402
+from validate_asn1 import validate_group  # noqa: E402
 
 os.chdir(script_dir)
 
